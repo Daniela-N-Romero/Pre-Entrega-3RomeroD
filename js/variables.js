4c92b684198a67717aa29e27efc88db780ab4638
Daniela Romero <danielanahirromero@gmail.com>
2022-11-15 20:18:51 -0300

    primer commit

---
+++ js/variables.js
@@ -0,0 +1,17 @@
+
+const carrito = []
+
+const macetas = [{ codigo: 1, nombre: 'Jardinera standard', imagen: '../assets/1.jpg', tamaño: '70 x 20 x 20 cm', peso: 10, precio: 1100},
+                { codigo: 2, nombre: 'Jardinera standard grande', imagen: '../assets/1.jpg', tamaño: '90 x 20 x 20 cm', peso: 14, precio: 1400},
+                { codigo: 3, nombre: 'Jardinera colgante', imagen: '../assets/2.jpg', tamaño: '40 x 20 x 20 cm', peso: 6, precio: 1000},
+                { codigo: 4, nombre: 'Jardinera colgante larga', imagen: '../assets/2.jpg', tamaño: '60 x 20 x 20 cm', peso: 8, precio: 1200},
+                { codigo: 5, nombre: 'Maceta cubo', imagen: '../assets/3.jpg', tamaño: '40 x 40 x 40 cm', peso: 14, precio: 1400},
+                { codigo: 6, nombre: 'Maceta cubo grande', imagen: '../assets/3.jpg', tamaño: '60 x 60 x 60 cm', peso: 17, precio: 1800},]
+
+
+const mensajeInicial = "Ingresa el código de la maceta que quieras agregar al carrito:" 
+
+
+const envioMinimo = 750
+const adicionalXKilo = 75
+
